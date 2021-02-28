84454064ac02f24caf526382d985c849d86e1156
huangweiyue <huangweiyue@quyiyuan.com>
2021-02-28 18:34:48 +0800

    代理模式

--- src/main/java/com/wondes/demo/enjoy/structure/proxy/ProxyClient.java
+++ src/main/java/com/wondes/demo/enjoy/structure/proxy/ProxyClient.java
@@ -3,7 +3,9 @@ package com.wondes.demo.enjoy.structure.proxy;
 import com.wondes.demo.enjoy.service.OrderService;
 
 /**
- * 代理模式
+ * 代理模式A不能直接访问C 通过B访问C
+ * 客户端无法直接调用OuterOrderService，所以做了代理ProxyOrder来中转
+ * 类似装饰器模式
  * 代售进口水果
  */
 public class ProxyClient {
@@ -11,7 +13,7 @@ public class ProxyClient {
         saveOrder();
     }
 
-    public static void saveOrder(){
+     private static void saveOrder(){
         //本地代理类
         OrderService orderService = new ProxyOrder();
 
