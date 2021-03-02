0b7c85ddafc3eb23d291a1fe792402df8bb85b78
huangweiyue <huangweiyue@quyiyuan.com>
2021-03-03 00:37:22 +0800

    责任链模式注释

--- src/main/java/com/wonders/demo/enjoy/action/chain/ChainCartTestClient.java
+++ src/main/java/com/wonders/demo/enjoy/action/chain/ChainCartTestClient.java
@@ -17,6 +17,7 @@ public class ChainCartTestClient {
 
     //初始化满减优惠券
     private static MultyDiscount multyDiscount = new FullMultyDiscount(null);
+
     static {
         multyDiscount = new NewerMultyDiscount(multyDiscount);
         multyDiscount = new SecondMultyDiscount(multyDiscount);
@@ -38,6 +39,9 @@ public class ChainCartTestClient {
         /**
          * TODO 注入优惠方案链条的第一个节点，断点打到此处，查看multyDiscount的值，
          * 实际上就是把多个MultyDiscount通过setNext的方式串起来，要注意先后顺序
+         * FullMultyDiscount，NewerMultyDiscount，SecondMultyDiscount，HolidayMultyDiscount继承MultyDiscount，
+         * MultyDiscount的构造方法实现的setNext操作，
+         *上面4个类中的calculate方法实现责任链模式中对money的处理逻辑
          */
         cart.setDiscount(multyDiscount);
         //这个方法内才会执行责任链模式的业务代码discount.calculate(money)
